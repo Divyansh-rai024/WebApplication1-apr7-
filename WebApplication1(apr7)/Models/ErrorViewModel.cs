9d34099c5d314c714cec8a312fe350304819fba0
divyansh <divyansh200224@gmail.com>
2026-04-07 09:48:45 +0530

    Add project files.

---
+++ WebApplication1(apr7)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1_apr7_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
